c49eda95f66c7210b6bee8056554969b83f81214
Lucas <tsaoyuan@gmail.com>
2022-10-13 14:58:42 +0800

    feat: Page live on WEB
    *.php use VSCode's Live Server & Firefox Live Server

---
+++ hello.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+    p{
+        background: #fa0;
+    }
+</style>
+<body>
+<h1><?php 
+echo 'hello everybod!';
+?></h1>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, sed ullam voluptate corporis atque nesciunt voluptas neque saepe iure totam laborum consequatur perferendis, dolore id blanditiis enim, quo provident animi!</p>
+
+<div>
+    <img src="https://picsum.photos/500/400/?random=8">
+</div>
+<div>
+    <img src="https://picsum.photos/500/400/?random=40">
+</div>
+<div>
+    <img src="https://picsum.photos/500/400/?random=40">
+</div>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, sed ullam voluptate corporis atque nesciunt voluptas neque saepe iure totam laborum consequatur perferendis, dolore id blanditiis enim, quo provident animi!</p>
+
+
+
+</body>
+</html>
+
+
