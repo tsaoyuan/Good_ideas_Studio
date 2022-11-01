4ca57a64776d7e147915cb831fb79d38d05ed715
Lucas <tsaoyuan@gmail.com>
2022-11-01 16:59:11 +0800

    feat: php strings methods: single quotes
    Everything in single quotes is strings.
    In single quotes use backslash(\) do escape.

--- phplessons/strings.php
+++ phplessons/strings.php
@@ -1,7 +1,12 @@
 <?php
     $name = "kikon";
 
-    echo "Hi, " . $name ."\n";
-    echo "My name is ". $name . "\n";
+    echo "Hi, " . $name .".\n";
+    echo "My name is ". $name . ".\n";
+    echo "My name is $name. ". "\n";
 
     
+    echo 'My name is $name.'.'\n'; 
+    echo "\n".'My name is $name.'."\n"; 
+    // escape 
+    echo "This is a Quotes \"hello word!\" \n";
