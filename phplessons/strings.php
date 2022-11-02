0bd32b835959d6dac1a3aec2780e5386a5db11fe
Lucas <tsaoyuan@gmail.com>
2022-11-02 10:21:25 +0800

    feat: php strings methods: Double quote
    在 Double quote 內，可使用變數($variable)、跳脫字元(\n, \t,..\\)
    要在變數的 "前" 或 "後" 加上字串，使用 {} 將變數框起來
    字串後，加上 PHP_EOL ，猶如 跳多字元的 \n

--- phplessons/strings.php
+++ phplessons/strings.php
@@ -10,3 +10,12 @@
     echo "\n".'My name is $name.'."\n"; 
     // escape 
     echo "This is a Quotes \"hello word!\" \n";
+
+    // {}
+    $juice = "apple";
+    echo "This juice is a {$juice}s. \n";
+    
+    // PHP_EOL
+    echo "Hi, what's up!". PHP_EOL;
+    echo "How do yu do, bro!". PHP_EOL;
+    
