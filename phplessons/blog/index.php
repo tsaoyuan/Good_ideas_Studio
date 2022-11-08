1a018cdb4f39a7e3a114da95e423bb073cf199ce
Lucas <tsaoyuan@gmail.com>
2022-11-08 15:43:44 +0800

    WelCome page's html design

--- phplessons/blog/index.php
+++ phplessons/blog/index.php
@@ -6,7 +6,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog</title>
 </head>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+    .wrap{
+        width: 300px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 20px;
+        /* border: 1px solid #f00; */
+
+    }
+    h1{
+        font-size: 36px;
+    }
+    .wrap ul li{
+        padding-top: 20px;
+        padding-left: 5px;
+    }
+    .wrap ul li .link{
+        font-size: 20px;
+    }
+</style>
 <body>
+    <div class="wrap">
+        <h1>WelCome Blog</h1>
+        <ul>
+            <li><a class="link" href="#">Sign Up</a></li>
+            <li><a class="link" href="#">Login</a></li>
+            
+        </ul>
+    </div>
     
 </body>
 </html>
