e12a4d337a65a4af4193bc6a4781681de363f78e
Lucas <tsaoyuan@gmail.com>
2022-11-10 10:07:30 +0800

    feat: Arithmetic opearators
    - full stop " . " use in string link string; number plus number use " + "
    - "n **2 " mean n*n; 5**2 = 5*5 = 25

---
+++ phplessons/operators.php
@@ -0,0 +1,24 @@
+<?php
+// Arithmetic
+echo 5.5 . PHP_EOL;
+echo 5 + 5 . PHP_EOL;
+
+$num = 5;
+$strlink = "$num" . "$num";
+echo $strlink .PHP_EOL;
+
+echo 5 - 6 .PHP_EOL;
+echo 5%4 .PHP_EOL;
+echo 5/4 .PHP_EOL;
+echo 5**2 .PHP_EOL;
+
+
+
+
+
+
+
+
+
+
+
