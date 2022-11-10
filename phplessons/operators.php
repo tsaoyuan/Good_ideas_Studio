487ae36a59615af1fa51f4060e7b4bbcc94c1a0f
Lucas <tsaoyuan@gmail.com>
2022-11-10 10:41:48 +0800

    feat: Comparison operators
    - comparison operators use three equal better than two equal sign
    - two equal sign (==) mean operator two varaible value
    - three equal sign (===) mean operator two varaible value and type
    - Ternary Operator (conditional ? true : false)

--- phplessons/operators.php
+++ phplessons/operators.php
@@ -12,7 +12,25 @@ echo 5%4 .PHP_EOL;
 echo 5/4 .PHP_EOL;
 echo 5**2 .PHP_EOL;
 
+// Comparison
+$x = 10;
+$y = "10";
 
+if($x == $y){
+    echo "True!" .PHP_EOL;
+}
+else{
+    echo "false!" .PHP_EOL;
+}
+
+if($x === $y){
+    echo "True!" .PHP_EOL;
+}
+else{
+    echo "false!" .PHP_EOL;
+}
+
+echo $x === $y ?  "True!" .PHP_EOL :  "false~~" .PHP_EOL;
 
 
 
