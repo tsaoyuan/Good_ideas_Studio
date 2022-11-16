c53748d6024c35bd7eb67d49d13da83515b2e780
Lucas <tsaoyuan@gmail.com>
2022-11-16 15:30:05 +0800

    feat: split style out of calculator.php
    - the css style is calc.css

--- phplessons/calculator.php
+++ phplessons/calculator.php
@@ -6,55 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculator</title>
 </head>
-<style>
-    *{
-        margin: 0;
-        padding: 0;
-        list-style: none;
-    }
-    .wrap{
-        width: 800px;
-        margin-right: auto;
-        margin-left: auto;
-        margin-top: 50px;
-    }
-
-    .wrap .insert, .wrap select, .wrap .insert, .wrap select option{
-        width: 200px;
-        padding: 10px 20px;
-        margin: 10px;
-        font-size: 36px;
-    }
-
-    button{
-        width: 200px;
-        margin-left: 10px;
-        margin-bottom: 10px;
-        margin-top: 10px;
-        font-size: 24px;
-    }
-    
-    .wrap p{
-        width: 400px;
-        margin-left: 10px;
-        margin-bottom: 10px;
-        font-size: 24px;
-    }
-    
-</style>
+<link rel="stylesheet" href="./calc.css">
 <body>
     <div class="wrap">   
         <form>
             <input class="insert" type="text" name="num1" placeholder="Number 1">
-            <input class="insert" type="text" name="num2" placeholder="Number 2">
             <select name="operator">
                 <option>None</option>
                 <option>Plus</option>
                 <option>Subtrct</option>
                 <option>Multily</option>
                 <option>Divide</option>
-
             </select>
+            <input class="insert" type="text" name="num2" placeholder="Number 2">
             </br>
             <button name="submoooit" value="submit" type="submit">Submit</button>
         </form>
