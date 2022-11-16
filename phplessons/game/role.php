b4a6ac44b967e338ded3c9a626e2deb7d68f2a86
Lucas <tsaoyuan@gmail.com>
2022-11-16 10:45:37 +0800

    feat: Properties use in private
    - "public" scope to make that property/method available from anywher
    - "private" scope when you want your property/method to be visible in its own class only.

--- phplessons/game/role.php
+++ phplessons/game/role.php
@@ -1,7 +1,7 @@
 <?php
 class User{
     // Properties
-    public $name;
+    private $name;
     public $hp;
     public $sp;
     public $attack;
@@ -9,29 +9,33 @@ class User{
     public $magic_attack;
     public $magic_defense;
     // Methods
-    function setName($newName){
+    public function setName($newName){
         $this -> name = $newName; 
     }
 
-    function __construct($name, $hp, $sp){
+    public function __construct($name, $hp, $sp){
         $this -> name = $name;
         $this -> hp = $hp;
         $this -> sp = $sp;
     }
     
+    public function getName(){
+        return $this-> name;
+    }
 }
 
 $player = new User('Lu-lala', 1000, 10);
-echo $player-> name .PHP_EOL;
+echo $player-> getName() .PHP_EOL;
 echo $player-> hp .PHP_EOL;
 echo $player-> sp .PHP_EOL;
 
+/*
 $Nobody = new User('xxx', 500, 0);
 $Nobody -> setName('Kine Knife');
 echo $Nobody-> name .PHP_EOL;
 echo $Nobody-> hp .PHP_EOL;
 echo $Nobody-> sp .PHP_EOL;
-
+*/
 
 
 
