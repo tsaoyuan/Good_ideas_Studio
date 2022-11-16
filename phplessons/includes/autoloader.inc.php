27f46aeb32285888777f5a93837e5b2c2c94cfcf
Lucas <tsaoyuan@gmail.com>
2022-11-16 23:19:37 +0800

    feat: OOP Calc autoloader use tran webpage
    - 不更換網頁的方法 → 變更為更換網頁

--- phplessons/includes/autoloader.inc.php
+++ phplessons/includes/autoloader.inc.php
@@ -2,6 +2,7 @@
 spl_autoload_register('myAutoLoader');
 
 function myAutoLoader($className){
+    /* no trns webpage
     $path = './classes/';
     $extension = '.class.php';
     $fullPath = $path. $className . $extension;
@@ -11,5 +12,23 @@ function myAutoLoader($className){
     }
 
     include_once $fullPath;
+    */
+
+    // trns webpage
+    $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+    if (strpos($url, 'includes')!== false){
+        $path = '../classes/';
+    }
+    else{
+        $path = './classes/';
+
+    }
+    $extension = '.class.php';
+
+    $fullPath = $path. $className . $extension;
+
+    require_once $fullPath;
+
 }
 
