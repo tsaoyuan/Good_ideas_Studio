abef9d2a33ec4a6fd4f7eebacb6696281e378862
Lucas <tsaoyuan@gmail.com>
2022-11-16 22:18:24 +0800

    feat: OOP Calc set calculator()
    - function trans to method
    - variable trans to Property

--- phplessons/classes/Calc.class.php
+++ phplessons/classes/Calc.class.php
@@ -11,36 +11,34 @@ class Calc{
         $this->num2 = $three;
     }
 
-    function calc(){
-        if(isset($_GET['submoooit'])){
-            $resultnum1 = $_GET['num1'];
-            $resultnum2 = $_GET['num2'];
-            $operator = $_GET['operator'];
-            
-            switch($operator){
+    public function calculator(){
+                
+            switch($this->operator){
                 case "None":
                     echo "Something wrrong! Yu need to select a method!".PHP_EOL;
                     break;
                 
                 case "Plus":
-                    echo $resultnum1 + $resultnum2 .PHP_EOL;
+                    $result = $this->num1 + $this->num2;
+                    return $result;
                     break;
                 
                 case "Subtrct":
-                    echo $resultnum1 - $resultnum2 .PHP_EOL;
+                    $result = $this->num1 - $this->num2;
+                    return $result;
                     break;
         
                 case "Multily":
-                    echo $resultnum1 * $resultnum2 .PHP_EOL;
+                    $result = $this->num1 * $this->num2;
+                    return $result;
                     break;
         
                 case "Divide":
-                    echo $resultnum1 / $resultnum2 .PHP_EOL;
+                    $result = $this->num1 / $this->num2;
+                    return $result;
                     break;
         
-            }
-        }
-    }
+                }
+     }
     
-
 }
