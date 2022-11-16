449b2a0310b4b601c5abdb7b85ec4adec7b8100f
Lucas <tsaoyuan@gmail.com>
2022-11-16 23:29:09 +0800

    feat: OOP Calc (int)$num1
    - post $num1 value is string, use (int) converting string to int
    - try catch, get TypeError Message

---
+++ phplessons/includes/calc.inc.php
@@ -0,0 +1,15 @@
+<?php
+    include 'autoloader.inc.php';
+
+    $oper = $_POST["operator"];
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+
+    $calc = new Calc($oper, (int)$num1, (int)$num2);
+    try{
+       echo $calc->calculator();
+
+    }catch(TypeError $e){
+        echo "ERROR: ".$e->getMessage();
+    }
+
