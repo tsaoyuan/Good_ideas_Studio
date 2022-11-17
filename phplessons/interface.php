9e58cdaafc41fb76a2f9268d4c03a25c7d32044e
Lucas <tsaoyuan@gmail.com>
2022-11-17 20:06:44 +0800

    feat: Class organize Interface
    - on line 56: action(AnimalInterface $animalType) mean $animalType type is Interface
    - on line 61: the instance is one of class in AnimalInterface
    - on line 65: Role's instance($someoneDo) use function of interface
    - $animalType don't use Normal class's instance, will get error message about "except $animalType is AnimalInterface ..."

--- phplessons/interface.php
+++ phplessons/interface.php
@@ -1,7 +1,6 @@
 <?php
 interface AnimalInterface{
     public function instinct();
-
 }
 
 class Bird implements AnimalInterface{
@@ -15,37 +14,52 @@ class Fish implements AnimalInterface{
     public function instinct(){
         // swim
         echo "I can swim!" .PHP_EOL;
-
     }
-    
 }
 
 class Lion implements AnimalInterface{
     public function instinct(){
         // run
         echo "I can run!" .PHP_EOL;
-
     }
-
 }
 
-$bird = new Bird();
-$lion = new Lion();
-$fish = new Fish();
-$animals = array($bird, $lion, $fish);
+// $bird = new Bird();
+// $lion = new Lion();
+// $fish = new Fish();
+// $animals = array($bird, $lion, $fish);
 
-foreach($animals as $animal) {
-    $animal->instinct();
-  }
+// foreach($animals as $animal) {
+//     $animal->instinct();
+//   }
 
 class AnimalAction {
+    
     public function action(AnimalInterface $animalType){
         $animalType->instinct();
     }
   }
 
-$animalType = new Fish();
-$someoneDo = new AnimalAction();
+// $animalType = new Fish();
+// $someoneDo = new AnimalAction();
+// $someoneDo->action($animalType);
+
+class Role {
+    public $name;
+    public function getNanme(){
+        return $this->name;
+    }
+    public function setName($newName){
+        $this->name = $newName;
+
+    }
+    public function action(AnimalInterface $animalType){
+        $animalType->instinct();
+    }
+  }
+
+$animalType = new Lion();
+$someoneDo = new Role();
+$someoneDo->setName("Ru Ru");
+echo "My name is " .$someoneDo->getNanme().".".PHP_EOL;
 $someoneDo->action($animalType);
-
-
