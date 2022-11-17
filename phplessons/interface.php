d861fb0e142035e29ed3379195bf453f50a16ce1
Lucas <tsaoyuan@gmail.com>
2022-11-17 15:30:15 +0800

    feat: Interface Group class method
    - Interface organize classes, like blueprint
    - In Interface's all methods must be public function
    - In interface, different classes must be use same method name

---
+++ phplessons/interface.php
@@ -0,0 +1,43 @@
+<?php
+interface AnimalInterface{
+    public function instinct();
+
+}
+
+class Bird implements AnimalInterface{
+    public function instinct(){
+        // fly
+        echo "I can fly!" .PHP_EOL;
+    }
+}
+
+class Fish implements AnimalInterface{
+    public function instinct(){
+        // swim
+        echo "I can swim!" .PHP_EOL;
+
+    }
+    
+}
+
+class Lion implements AnimalInterface{
+    public function instinct(){
+        // run
+        echo "I can run!" .PHP_EOL;
+
+    }
+
+}
+
+$bird = new Bird();
+$lion = new Lion();
+$fish = new Fish();
+$animals = array($bird, $lion, $fish);
+
+foreach($animals as $animal) {
+    $animal->instinct();
+  }
+
+
+
+
