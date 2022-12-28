dc676719dc5fd2bb87a8df29512c64d1c2dbf32e
Lucas <tsaoyuan@gmail.com>
2022-12-28 11:53:28 +0800

    feat: Page Links

--- phplessons/blog/about.view.php
+++ phplessons/blog/about.view.php
@@ -138,12 +138,12 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">About us</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <p>Hello Word!</p>
+                <p>This is About page.</p>
             </div>
         </main>
     </div>
--- phplessons/blog/contact.view.php
+++ phplessons/blog/contact.view.php
@@ -138,12 +138,12 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <p>Hello Word!</p>
+                <p>Contact in here!</p>
             </div>
         </main>
     </div>
