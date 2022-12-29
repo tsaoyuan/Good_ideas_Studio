82e099804a22c354b5f897f3ff1a5fd9cf7c0ec8
Lucas <tsaoyuan@gmail.com>
2022-12-29 12:45:34 +0800

    feat: Move controller router in router.php
    - Use routerToController() to switch router
    - Use abort(), catch $code and report page. defult is 404

--- phplessons/blog/index.php
+++ phplessons/blog/index.php
@@ -1,18 +1,3 @@
 <?php
 require './function.php';
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'] ;
-// echo $uri;
-
-$routers = [
-    '/' => './controllers/index.php',
-    '/about' => './controllers/about.php',
-    '/contact' => './controllers/contact.php',
-];
-
-if(array_key_exists($uri, $routers)){
-    require  $routers[$uri];
-}else{
-    http_response_code(404);
-    require './views/404.php';
-}
+require './router.php';
