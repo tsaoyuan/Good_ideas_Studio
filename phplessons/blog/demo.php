7471ce650fb1559d750fc9780b8622a9fc22ade5
Lucas <tsaoyuan@gmail.com>
2023-01-03 11:21:57 +0800

    feat: Singleton use static methods get instance
    - Static Property 要用 `self::Property`, 取得 class 本身的 Property
    - Singleton class 起手勢:
       - `private static $instance = null`
       - `private function __construct`
       -  `public static function getInstance()`
    - 執行 Singleton class by static methods 過程中，主要產生 PDO instance 在 `getInstance()`，`__construct()` 在於設定 instance 的 property values
    - 在`__construct()` 產生 PDO instance 是無作用
    - 因此，將 PDO instance 移置 `getInstance()` 內，"SELECT * FROM users"
      is sucess

--- phplessons/blog/demo.php
+++ phplessons/blog/demo.php
@@ -1,47 +1,40 @@
 <?php
 require './function.php';
 
-class Conn extends PDO{
-// class Conn{
-    // private $dsn, $user, $pwd;
+// class Conn extends PDO{
+class Conn{
     private static $dsn, $user, $pwd;
     private static $instance = null;
 
     private function __construct()
     {
-        // $this->dsn = "mysql:host=localhost;dbname=blog";
-        // $this->user = "luna";
-        // $this->pwd = "0000";
-
+        
+    }
+    
+    public static function getInstance()
+    {
         self::$dsn = "mysql:host=localhost;dbname=blog";
         self::$user = "luna";
         self::$pwd = "0000";
-        // echo "$this->dsn and $this->user and $this->pwd";
-        try{
-            echo "3 Any body in Construct?".PHP_EOL;
-            // parent::__construct($this->dsn, $this->user, $this->pwd);
-            parent::__construct(self::$dsn, self::$user, self::$pwd);
 
-            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }catch(PDOException $e){
-            
-            echo "Connection failed: ".$e->getMessage();
-        }
-    }
-
-    public static function getInstance()
-    {
-        echo "1 進入 getInstance".PHP_EOL;
+        echo "1 進入 getInstance()".PHP_EOL;
         if (!self::$instance) {
-            echo '2 !self::$instance == true' . PHP_EOL;
-            self::$instance = new static;
+            echo "2 !self::instance == true" . PHP_EOL;
+            // self::$instance = new static;
+            try{
+                self::$instance = new PDO(self::$dsn, self::$user, self::$pwd);
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            }catch(PDOException $e){
+                echo "Connection failed: " . $e->getMessage();
+            }
         }
         return self::$instance;
     }
 }
 
 $conn = Conn::getInstance();
-// var_dump($conn);
+echo "var_dump: ";var_dump($conn);
 $statement = $conn->prepare("SELECT * FROM users");
 $statement->execute();
 // var_dump($statement);
