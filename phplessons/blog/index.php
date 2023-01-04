ca8a38475390669d43adab309633d9962d108715
Lucas <tsaoyuan@gmail.com>
2023-01-04 18:51:43 +0800

    feat: SQL injection
    - $sql haven't formatted query, then SQL injection
    - 有心人士透過加料網址列，產生 SQL injection
    - http://localhost:8888/?id=12; DELETE FROM Userswhere id = 12; 雖然 $_GET["12"] 撈出 id = 12 的資料，但是撈出資料後，馬上透過 DELETE … 刪除該筆資料. 若是 DROP TABLE，TABLE 就會整個被刪除

--- phplessons/blog/index.php
+++ phplessons/blog/index.php
@@ -5,5 +5,8 @@ require './ConnSingleton.php';
 require './Database.php';
 
 $db = new Database();
-$posts = $db->query("SELECT * FROM users")->fetchAll(PDO::FETCH_ASSOC); 
+// $posts = $db->query("SELECT * FROM users")->fetchAll(PDO::FETCH_ASSOC); 
+$id = $_GET["id"];
+$sql = "SELECT * FROM Users where id = {$id}";
+$posts = $db->query($sql)->fetch(PDO::FETCH_ASSOC); 
 dumpDie($posts);
