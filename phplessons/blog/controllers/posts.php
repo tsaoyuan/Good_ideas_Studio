7260087c6c7d103b44ce7f0b28925c2b0776ad9d
Lucas <tsaoyuan@gmail.com>
2023-01-05 16:50:14 +0800

    issue 002: Class `Database` no fund in posts.php
    - posts.php can't catch Class `Database`, then dumpDie($db) can't view
      result

--- phplessons/blog/controllers/posts.php
+++ phplessons/blog/controllers/posts.php
@@ -2,3 +2,5 @@
 $heading = 'Posts';
 require '/var/www/html/phplessons/blog/views/posts.view.php';
 
+$db = new Database();
+dumpDie($db);
--- phplessons/blog/index.php
+++ phplessons/blog/index.php
@@ -4,9 +4,3 @@ require './router.php';
 require './ConnSingleton.php';
 require './Database.php';
 
-$db = new Database();
-$id = $_GET["id"];
-// a named parameter like :id
-$sql = "SELECT * FROM Users where id = :id";
-$posts = $db->query($sql, [':id' => $id])->fetch(PDO::FETCH_ASSOC); 
-dumpDie($posts);
