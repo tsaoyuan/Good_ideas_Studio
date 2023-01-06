90f1849a4418f9e65da5fb98a594d4cabc8547f4
Lucas <tsaoyuan@gmail.com>
2023-01-06 16:48:17 +0800

    feat: 細分不符合搜尋條件的結果
    - Database() add try catch
    - 新增權限不足之 403.php

--- phplessons/blog/controllers/post.php
+++ phplessons/blog/controllers/post.php
@@ -1,9 +1,23 @@
 <?php
 $db = new Database();
 
-$post = $db->query("SELECT * FROM Posts where id = :id", [':id' => $_GET['id']])->fetch();
+// $post = $db->query("SELECT * FROM Posts where Uid = :Uid and Id = :Id", [
+//     ':Uid' => 'John666',
+//     ':Id' => $_GET['id']
+//     ])->fetch();
 
-// dumpDie($post);
+$post = $db->query("SELECT * FROM Posts where Id = :Id", [
+    ':Id' => $_GET['id']
+    ])->fetch();
+
+// 不符合搜尋條件
+if (! $post){
+    abort(404);
+}
+// 細分不符合搜尋條件的結果
+if($post['Uid'] !== 'John666'){
+    abort(403);
+}
 
 $heading = 'Post';
 require __DIR__.'/../views/post.view.php';
