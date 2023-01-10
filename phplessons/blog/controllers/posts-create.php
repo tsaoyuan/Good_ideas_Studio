88c1da45ab0698c8ac6274c547c890ce21353298
Lucas <tsaoyuan@gmail.com>
2023-01-10 16:49:31 +0800

    feat: Views for posts-create
    - form method="POST", then dumpDie($_POST) get `<form>` post imformation
    - `<form>` post imfomation is arrary, array key is every html tag name's
      value; array value is post imformation at <textarea>
      -ex. html tag `<textarea>`, array key is `title`; array value is
    whatever

--- phplessons/blog/controllers/posts-create.php
+++ phplessons/blog/controllers/posts-create.php
@@ -1,4 +1,7 @@
 <?php
 
 $heading = 'Create Post';
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    dumpDie($_POST);
+}
 require __DIR__.'/../views/posts-create.view.php';
--- phplessons/blog/views/posts-create.view.php
+++ phplessons/blog/views/posts-create.view.php
@@ -1,10 +1,20 @@
-<?php require __DIR__.'/partials/head.php'; ?>
-<?php require __DIR__.'/partials/nav.php'; ?>
-<?php require __DIR__.'/partials/banner.php'; ?>
+<?php require __DIR__ . '/partials/head.php'; ?>
+<?php require __DIR__ . '/partials/nav.php'; ?>
+<?php require __DIR__ . '/partials/banner.php'; ?>
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <h1>Create a post</h1>
+        <form method="POST">
+            <label for="title" class="block">Title:</label>
+            <textarea id="title" type="text" name="title" placeholder="Your Title"></textarea>
+
+            <button type="submit" class="block mt-5 hover:underline">Submit post</button>
+        </form>
+
+        <span class="mt-10 inline-block">
+            <a href="/posts" class="text-red-500">Go back Posts</a>
+        </span>
     </div>
 </main>
 
-<?php require __DIR__.'/partials/foot.php'; ?>
+<?php require __DIR__ . '/partials/foot.php'; ?>
