c684eeadbfec2a0700258a729ff209d22e92d4ee
Lucas <tsaoyuan@gmail.com>
2023-01-10 22:03:47 +0800

    feat: Validation empty input
    - check title empty input
    - empty(variable) check weather a `variable` is empty or not
    - title have words; empty($errors) is true, then insert into value

--- phplessons/blog/controllers/posts-create.php
+++ phplessons/blog/controllers/posts-create.php
@@ -1,10 +1,23 @@
 <?php
 $db = new Database();
 $heading = 'Create Post';
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $db->query("INSERT INTO `Posts` (`Uid`, `Title`) VALUES (:Uid, :Title)", [
-        ":Uid" => "Andy Run",
-        ":Title" => $_POST['title']
-    ]);
+// before insert into check
+    $errors = [];
+    // check title empty input
+    if(strlen($_POST['title']) == 0){
+        $errors['title'] = 'Title is required!'; 
+    }
+
+    // title have words, then insert into value
+    if(empty($errors)){
+        
+        $db->query("INSERT INTO `Posts` (`Uid`, `Title`) VALUES (:Uid, :Title)", [
+            ":Uid" => "Andy Run",
+            ":Title" => $_POST['title']
+        ]);
+    }
+
 }
 require __DIR__.'/../views/posts-create.view.php';
