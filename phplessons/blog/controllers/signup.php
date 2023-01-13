7b63e1719cfc601dc7b6ec6bd479972addb9f055
Lucas <tsaoyuan@gmail.com>
2023-01-13 12:24:20 +0800

    feat: SignUp Setting
    - sign up `Pid` and `Pwd`
    - alter table column datatype: `Name` trans to NULL

--- phplessons/blog/controllers/signup.php
+++ phplessons/blog/controllers/signup.php
@@ -1,3 +1,27 @@
 <?php
+require __DIR__.'/../Validator.php';
+$db = new Database();
 $heading = 'Sign Up';
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+// before insert into check
+    $errors = [];
+
+    // mearge title empty input and title maximum number of characters
+    if(!Vaildator::string($_POST['uid'], 1, 25) || !Vaildator::string($_POST['pwd'], 1, 25)){
+        $errors['emptyInput'] = "Fill in all Fields";
+       
+    }
+
+    if(empty($errors)){
+        
+        $db->query("INSERT INTO `Users` (`Uid`, `Pwd`) VALUES (:Uid, :Pwd)", [
+            ":Uid" => $_POST['uid'],
+            ":Pwd" => $_POST['pwd']
+        ]);
+    }
+
+}
+
 require __DIR__.'/../views/signup.view.php';
--- phplessons/blog/views/signup.view.php
+++ phplessons/blog/views/signup.view.php
@@ -4,14 +4,20 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <p>This is SignUp page.</p>
-        <form mathod="POST">
+        <form method="POST">
             <ul>
-                
-                <li class="mt-3"><label for="uid" class="block">Uid:</label><input id="uid" type="text" name="uid" placeholder="User Name"><?= $_POST['uid'] ?? ""?></li>
+                <li class="mt-3"><label for="uid" class="block">Uid:</label><input id="uid" type="text" name="uid" placeholder="User Name"></li>
 
-                
-                <li class="mt-3"><label for="pwd" class="block">Password:</label><input id="pwd" type="password" name="pwd" placeholder="password"><?= $_POST['pwd'] ?? ""?></li>
+
+                <li class="mt-3"><label for="pwd" class="block">Password:</label><input id="pwd" type="password" name="pwd" placeholder="password"></li>
             </ul>
+
+            <?php if (isset($errors['emptyInput'])) : ?>
+
+                    <p class="text-red-500"><?= $errors['emptyInput'] ?></p>
+
+            <?php endif; ?>
+
             <button type="submit" class="block mt-3 hover:underline">Sign up</button>
         </form>
     </div>
