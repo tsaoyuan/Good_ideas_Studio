4c73ff4b58a6c2878660f0f468f2d9a63bbed8f4
Lucas <tsaoyuan@gmail.com>
2023-01-16 14:07:00 +0800

    feat: Set Autoload in public/index.php
    - 重構 `/public/index.php`
      - 每當專案內新增一 class 便要在 `/public/index.php` 再新增 `require base_path('ClassWhaterer.php');
    - 將 `require base_path(...)` 這項重複做的事情，用原生 PHP 方法 `spl_autoload_register()`處理
    - **當物件的初始化，延遲到程式需要使用時再進行初始化，而不是一開始就初始化; 又稱 Lazy loading**，有助於提升程式的效能和效率。
      - `spl_autoload_register()` 自動觸發頁面需要用到的 class
    
    - class name: class Conn{}; file name: ConnSingleton
      - class name 和 base_path($class,'.php') 對應不上，導致至應用程式，在該路徑`/var/../Conn.php` 找不到與 `class Conn` 對應的檔案
    - 變更 file name 為 `Conn.php`

--- phplessons/blog/public/index.php
+++ phplessons/blog/public/index.php
@@ -2,9 +2,15 @@
 const BASE_PATH = __DIR__.'/../';
 require BASE_PATH.'function.php';
 
-// var_dump(base_path("views/index.view.php"));
-require base_path('ConnSingleton.php');
-require base_path('Database.php');
-require base_path('Response.php');
+// require base_path('ConnSingleton.php');
+// require base_path('Database.php');
+// require base_path('Response.php');
+spl_autoload_register(function($class){
+    // dumpDie(base_path($class.'.php')); 
+    //--> "/var/www/html/phplessons/blog/public/../Database.php"
+
+    // require $class.'.php';
+    require base_path($class.'.php');
+});
 require base_path('router.php');
 
