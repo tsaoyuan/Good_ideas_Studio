6b79890d4aef3e7f134187305f822980060d35ce
Lucas <tsaoyuan@gmail.com>
2023-01-16 15:56:28 +0800

    feat: Add /Core 用來集中放 ClassName.php
    - 檔案移位，遂變更 /public/index.php `require
      base_path("/Core/{$class}.php")`

--- phplessons/blog/public/index.php
+++ phplessons/blog/public/index.php
@@ -2,15 +2,13 @@
 const BASE_PATH = __DIR__.'/../';
 require BASE_PATH.'function.php';
 
-// require base_path('ConnSingleton.php');
-// require base_path('Database.php');
-// require base_path('Response.php');
 spl_autoload_register(function($class){
     // dumpDie(base_path($class.'.php')); 
     //--> "/var/www/html/phplessons/blog/public/../Database.php"
 
-    // require $class.'.php';
-    require base_path($class.'.php');
+    // require 'Core/'.$class.'.php';
+    // require base_path('Core/'.$class.'.php');
+    require base_path("/Core/{$class}.php");
 });
 require base_path('router.php');
 
