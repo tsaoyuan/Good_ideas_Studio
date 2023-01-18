90d3decb094ab4ddb250052783306746f5f8d2fb
Lucas <tsaoyuan@gmail.com>
2023-01-18 17:57:45 +0800

    feat: Set SESSION for login use
    - `authorizeLogin()` check this user have signup
    - `$user`  user have already signup, user imformation set in $user
    - 啟動 session: `session_start();`
    - 將 MySQL 撈出的資訊($user["Uid"],
      $user["Pwd"])，分別設定為($_SESSION["uid"], $_SESSION["pwd"])
    - user 登入成功 跳轉首頁

--- phplessons/blog/controllers/login.php
+++ phplessons/blog/controllers/login.php
@@ -18,8 +18,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         ':Uid' => $currentUid,
         ':Pwd' => $currentPwd
         ])->findOrFail();
-    
+
+        // check this user have signup
         authorizeLogin($post['Uid'] == $currentUid, $post['Pwd'] == $currentPwd);
+        
+        // user have already signup, user imformation set in $user
+        $user = $db->query("SELECT * FROM Users where Uid = :Uid and Pwd = :Pwd", [
+        ':Uid' => $currentUid,
+        ':Pwd' => $currentPwd
+        ])->find();
+
+        // dumpDie($user["Uid"]);
+        session_start();
+        // user imformation set in $_SESSION["uid"] and $_SESSION["pwd"]
+        $_SESSION["uid"] = $user["Uid"]; 
+        $_SESSION["pwd"] = $user["Pwd"]; 
+        // dumpDie($_SESSION);
     }
 
 
