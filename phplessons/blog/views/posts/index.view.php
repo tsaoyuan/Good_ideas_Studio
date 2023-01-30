a5b4a2ee6cfaecada45eff1029b3d3b584a30b78
Lucas <tsaoyuan@gmail.com>
2023-01-30 17:08:55 +0800

    feat: `index.view.php` 顯示 Admin Post
    -1. 登入 Admin 時，顯示 Posts 內所有文章，不分使用者
    -2. 透過`index.view.php` 的 $post["Uid"] === $_SESSION["uid"]為 true, 即當前為 Admin
    -3. Admin 皆作為超連結顯示; 非 Admin 皆為一般文字顯示
    - 暫時註解未使用的 HTML 區塊

--- phplessons/blog/views/posts/index.view.php
+++ phplessons/blog/views/posts/index.view.php
@@ -1,13 +1,16 @@
-<?php require __DIR__.'/../partials/head.php'; ?>
-<?php require __DIR__.'/../partials/nav.php'; ?>
-<?php require __DIR__.'/../partials/banner.php'; ?>
+<?php require __DIR__ . '/../partials/head.php'; ?>
+<?php require __DIR__ . '/../partials/nav.php'; ?>
+<?php require __DIR__ . '/../partials/banner.php'; ?>
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <?php foreach($posts as $post) : ?>
+        <?php foreach ($posts as $post) : ?>
             <li>
-                <a class="hover:text-orange-400 hover:underline" href="/post?id=<?= $post['Id'] ?>">
-                <?=  htmlspecialchars($post['Title']) ?></a>
+                <?php if ($post["Uid"] === $_SESSION["uid"]) : ?>
+                    <a class="underline hover:text-orange-400" href="/post?id=<?= $post['Id'] ?>">
+                <?php endif ?>
+
+                <?= htmlspecialchars($post['Title']) ?></a>
             </li>
 
         <?php endforeach; ?>
@@ -15,7 +18,7 @@
             <span class="mt-5 p-4 inline-block hover:underline">
                 <a href="/" class="text-red-500">Go back Home</a>
             </span>
-    
+
             <span class="mt-5 p-5 inline-block hover:underline">
                 <a href="/posts/create" class="text-blue-700">Create Post</a>
             </span>
@@ -23,4 +26,4 @@
     </div>
 </main>
 
-<?php require __DIR__.'/../partials/foot.php'; ?>
+<?php require __DIR__ . '/../partials/foot.php'; ?>
