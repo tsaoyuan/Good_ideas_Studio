0ba75d812eaa434eac20bb366b299cadcc2444c6
Lucas <tsaoyuan@gmail.com>
2023-02-03 16:56:03 +0800

    feat: Show Users tables on Manage Account Page

--- phplessons/blog/Models/User.php
+++ phplessons/blog/Models/User.php
@@ -77,3 +77,4 @@ class User{
             break;
         }
     }
+}
--- phplessons/blog/views/users/index.view.php
+++ phplessons/blog/views/users/index.view.php
@@ -4,19 +4,64 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <?php foreach ($users as $user) : ?>
+        <!-- 
             <li>
-                <!-- 
-                    <?php if ($user["Uid"] === $_SESSION["uid"]) : ?>
+                <?php if ($user["Uid"] === $_SESSION["uid"]) : ?>
                     <a class="underline hover:text-orange-400" href="/post?id=<?= $user['Id'] ?>">
-                    <?php endif ?> 
-                -->
+                        <?php endif ?> 
+                        
+                    </a> 
+                </li>
+         -->
+        <table class="table-auto">
+            <tr>
+                <thead>
+                    <!-- <th class="w-2/10">Uid</th>
+                    <th class="w-4/10">Email</th>
+                    <th class="w-1/10">Role</th>
+                    <th class="w-3/10">Function</th> -->
 
-                 <?= htmlspecialchars($user['Uid']) ?>
-                <!-- </a> -->
-            </li>
+                    <th>Uid</th>
+                    <th></th>
+                    <th>Email</th>
+                    <th></th>
+                    <th>Role</th>
+                    <th></th>
+                    <th>Function</th>
+                    <th></th>
+                </thead>
+            </tr>
+            <tbody>
+                <?php foreach ($users as $user) : ?>
+                    <tr>
+                        <td>
+                            <!-- <td class="w-2/10"> -->
+                            <?= htmlspecialchars($user['Uid']) ?>
+                        </td>
+                        <td></td>
 
-        <?php endforeach; ?>
+                        <!-- <td class="w-4/10"> -->
+                        <td>
+                            <?= htmlspecialchars($user['email']) ?>
+                        </td>
+                        <td></td>
+                        <!-- <td class="w-1/10"> -->
+                        <td>
+                            <?= htmlspecialchars($user['Role']) ?>
+                        </td>
+                        <td></td>
+
+                        <!-- <td class="w-3/10"> -->
+                        <td>
+                            <a href="#" class="text-blue-500 hover:underline">Edit</a>
+                        </td>
+                        <td></td>
+                    </tr>
+
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
         <!--
         <section class="btn">
             <span class="mt-5 p-4 inline-block hover:underline">
