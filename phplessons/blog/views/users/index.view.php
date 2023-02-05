e122d91eaf03dbf766c195770cb257e461538a76
Lucas <tsaoyuan@gmail.com>
2023-02-05 20:55:21 +0800

    feat: Edit User Role Success
    - Use `$_POST["role"]` Get Selected Values from Select Option
    - 刪除 `index.view.php` 無作用程式

--- phplessons/blog/views/users/index.view.php
+++ phplessons/blog/views/users/index.view.php
@@ -5,23 +5,9 @@
 <main>
     <form method="POST">
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <!-- 
-            <li>
-                <?php if ($user["Uid"] === $_SESSION["uid"]) : ?>
-                    <a class="underline hover:text-orange-400" href="/post?id=<?= $user['Id'] ?>">
-                        <?php endif ?> 
-                        
-                    </a> 
-                </li>
-         -->
             <table class="table-auto">
                 <tr>
                     <thead>
-                        <!-- <th class="w-2/10">Uid</th>
-                    <th class="w-4/10">Email</th>
-                    <th class="w-1/10">Role</th>
-                    <th class="w-3/10">Function</th> -->
-
                         <th>Uid</th>
                         <th></th>
                         <th>Email</th>
@@ -36,30 +22,23 @@
                     <?php foreach ($users as $user) : ?>
                         <tr>
                             <td>
-                                <!-- <td class="w-2/10"> -->
                                 <?= htmlspecialchars($user["Uid"]) ?>
 
                                 <input name="id" value="<?= $user["Id"] ?>">
-
                             </td>
                             <td></td>
 
-                            <!-- <td class="w-4/10"> -->
                             <td>
                                 <?= htmlspecialchars($user['email']) ?>
                             </td>
                             <td></td>
-                            <!-- <td class="w-1/10"> -->
                             <td>
                                 <?= htmlspecialchars($user['Role']) ?>
                             </td>
                             <td></td>
 
-                            <!-- <td class="w-3/10"> -->
                             <td>
-                                <a href="/user?id=<?= $user["Id"]?>" class="text-blue-500 hover:underline">Edit</a>
-                                <!-- <button type="submit" name="edit" class="block text-blue-500 hover:underline">Edit</button> -->
-                                <!-- <input type="submit" name="edit" vlaue="Edit" class="block text-blue-500 hover:underline"> -->
+                                <a href="/user?id=<?= $user["Id"] ?>" class="text-blue-500 hover:underline">Edit</a>
                             </td>
                             <td></td>
                         </tr>
@@ -68,17 +47,12 @@
                 </tbody>
             </table>
 
-            <!--
-        <section class="btn">
-            <span class="mt-5 p-4 inline-block hover:underline">
-                <a href="/" class="text-red-500">Go back Home</a>
-            </span>
+            <section class="btn">
+                <span class="mt-5 p-4 inline-block hover:underline">
+                    <a href="/" class="text-red-500">Go back Home</a>
+                </span>
+            </section>
 
-            <span class="mt-5 p-5 inline-block hover:underline">
-                <a href="/posts/create" class="text-blue-700">Create Post</a>
-            </span>
-        </section>
-    -->
         </div>
     </form>
 </main>
