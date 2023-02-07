59f64df818200815285cae664c15206fcc09890d
Lucas <tsaoyuan@gmail.com>
2023-02-08 06:17:38 +0800

    feat: 新增 Sign Up Sucess 即轉址至 Login
    - 新增 Sign Up Sucess 訊息，避免語意上的錯亂，遂用 `$message` 替代 `$error`
    - foreach 輸出 `$message` 陣列內的 `$context`

--- phplessons/blog/controllers/users/create.php
+++ phplessons/blog/controllers/users/create.php
@@ -1,32 +1,37 @@
 <?php
-require base_path('Core/Validator.php');
+require base_path("Core/Validator.php");
 use Core\Database;
 use Core\Vaildator;
 
 $db = new Database();
-$errors = [];
-
+$message = [];
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     // before insert into check
     
     // mearge title empty input and title maximum number of characters
-    if(!Vaildator::string($_POST['uid'], 1, 25) || !Vaildator::string($_POST['pwd'], 1, 25)){
-        $errors['emptyInput'] = "Fill in all Fields";
-        
+    if(!Vaildator::string($_POST["uid"], 1, 25) || !Vaildator::string($_POST['pwd'], 1, 25)){
+        $message["emptyInput"] = "Fill in all Fields";
     }
     
-    if(empty($errors)){
+    if(!Vaildator::string($_POST["pwd"], 1, 4)){
+        $message["pwdOverLength"] = "Password Over 4 Code";
+    }
+
+    if(empty($message)){
         
         $db->query("INSERT INTO `Users` (`Uid`, `Pwd`) VALUES (:Uid, :Pwd)", [
-            ":Uid" => $_POST['uid'],
-            ":Pwd" => $_POST['pwd']
+            ":Uid" => $_POST["uid"],
+            ":Pwd" => $_POST["pwd"]
         ]);
+
+        $message["SignUpSucess"] = "Sign Up Sucess.. wait 2 sceond";
+        // dumpDie($message);
+        header("Refresh: 2; url=/login");
     }
     
 }
 
-// require __DIR__.'/../views/signup.view.php';
 view("users/create.view.php", [
-    'heading' => 'Sign Up',
-    'errors'=> $errors 
+    "heading" => 'Sign Up',
+    "message"=> $message 
 ]);
